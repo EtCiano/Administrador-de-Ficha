92d08eb79bef0668dcc92b1ff84ae045fc6cfce1
EtCiano <miguelfmellobirck@gmail.com>
2026-04-20 10:49:23 -0300

    Refactor FichaRPG.py for improved attribute handling

--- FichaRPG.py
+++ FichaRPG.py
@@ -34,113 +34,6 @@ def salvar_ficha():
             f.write(linha)
     mostrar_atributos()
 
-if Path(ficha).is_file():
-    linhas = pegar_ficha()   
-
-ctk.set_appearance_mode("System")
-ctk.set_default_color_theme("blue")
-
-def alterar_atributo_valor(atributo, valor):
-    linhas[atributo] = str(int(linhas[atributo])+int(valor)) + '\n'
-    salvar_ficha()
-
-def alterar_atributo(indice):
-    def salvar(valor):
-        linhas[indice] = valor + '\n'
-        salvar_ficha()
-        janela.destroy()
-    janela = ctk.CTkToplevel(app)
-    janela.title("Alterar Atributo")
-    janela.geometry("300x400")
-    janela.grab_set()
-    label = ctk.CTkLabel(janela, text=locais[indice].capitalize() + ": " + linhas[indice].strip(), anchor="w")
-    label.pack(pady=10, fill="x", padx=10)
-    entrada = ctk.CTkEntry(janela, placeholder_text="Valor")
-    entrada.pack(pady=10)
-    botao = ctk.CTkButton(janela, text="Alterar", command=lambda: salvar(entrada.get()))
-    botao.pack(pady=10)
-
-def editar_ficha_toda():
-    janela = ctk.CTkToplevel(app)
-    janela.title("Alterar Ficha")
-    janela.geometry("300x400")
-    janela.grab_set()
-    label_titulo = ctk.CTkLabel(janela, text="Criar ficha")
-    label_titulo.pack(pady=10)
-
-    scroll = ctk.CTkScrollableFrame(janela)
-    scroll.pack(fill="both", expand=True, padx=10)
-
-    entries = []
-    for campo in locais:
-        label = ctk.CTkLabel(scroll, text=campo.capitalize())
-        label.pack(anchor="w")
-
-        entry = ctk.CTkEntry(scroll, placeholder_text=campo.capitalize())
-        entry.pack(fill="x", pady=(0, 8))
-
-        entries.append(entry)
-    def salvar():
-        global linhas
-        with open(ficha, 'w') as f:
-            for entry in entries:
-                f.write(entry.get() + '\n')
-        linhas = pegar_ficha()
-        mostrar_atributos()
-        janela.destroy()
-
-    botao_ok = ctk.CTkButton(janela, text="OK", command=salvar)
-    botao_ok.pack(pady=10)
-
-app = ctk.CTk()
-app.title("Administrador de ficha (WAR)")
-
-frame_atributos = ctk.CTkScrollableFrame(master=app)
-frame_atributos.pack(pady=20, padx=(20, 10), fill="both", expand=True, side="left")
-
-#region frame de controles
-frame_controles = ctk.CTkScrollableFrame(master=app)
-
-#region mudar vida
-frame_mudar_vida = ctk.CTkFrame(frame_controles, border_width=2, border_color="gray")
-frame_mudar_vida.pack(pady=10, padx=10, ipadx=10, ipady=10)
-
-ctk.CTkLabel(frame_mudar_vida, text="Alterar Vida", anchor="w").pack(pady=(5, 0), fill="x", padx=10)
-
-entrada_vida = ctk.CTkEntry(frame_mudar_vida, placeholder_text="Valor")
-entrada_vida.pack(pady=10)
-
-botao_vida = ctk.CTkButton(frame_mudar_vida, text="Mudar Vida", command=lambda: alterar_atributo_valor(2, entrada_vida.get()))
-botao_vida.pack(pady=10)
-#endregion
-#region mudar san
-frame_mudar_sanidade = ctk.CTkFrame(frame_controles, border_width=2, border_color="gray")
-frame_mudar_sanidade.pack(pady=10, padx=10, ipadx=10, ipady=10)
-
-ctk.CTkLabel(frame_mudar_sanidade, text="Alterar Sanidade", anchor="w").pack(pady=(5, 0), fill="x", padx=10)
-
-entrada_sanidade = ctk.CTkEntry(frame_mudar_sanidade, placeholder_text="Valor")
-entrada_sanidade.pack(pady=10)
-
-botao_sanidade = ctk.CTkButton(frame_mudar_sanidade, text="Mudar Sanidade", command=lambda: alterar_atributo_valor(4, entrada_sanidade.get()))
-botao_sanidade.pack(pady=10)
-#endregion
-#region mudar atributo
-frame_mudar_atributo = ctk.CTkFrame(frame_controles, border_width=2, border_color="gray")
-frame_mudar_atributo.pack(pady=10, padx=10, ipadx=10, ipady=10)
-
-ctk.CTkLabel(frame_mudar_atributo, text="Alterar Atributo", anchor="w").pack(pady=(5, 0), fill="x", padx=10)
-
-selecao = ctk.CTkOptionMenu(frame_mudar_atributo, values=locais)
-selecao.pack(pady=10)
-
-mudar_atributo_button = ctk.CTkButton(frame_mudar_atributo, text="Mudar Atributo", command=lambda: alterar_atributo(locais.index(selecao.get())))
-mudar_atributo_button.pack(pady=10)
-#endregion
-
-frame_controles.pack(pady=20, padx=(10, 20), fill="both", expand=True, side="left")
-#endregion
-
 def mostrar_atributos():
     if len(linhas) < len(locais):
         return
@@ -164,6 +57,106 @@ def mostrar_atributos():
             continue
         label = ctk.CTkLabel(frame_atributos, text=locais[i].capitalize() + ": " + linhas[i].strip(), anchor="w")
         label.pack(fill="x")
+
+def alterar_atributo_valor(atributo, valor):
+    linhas[atributo] = str(int(linhas[atributo])+int(valor)) + '\n'
+    salvar_ficha()
+
+def alterar_atributo(indice):
+    def salvar(valor):
+        linhas[indice] = valor + '\n'
+        salvar_ficha()
+        janela.destroy()
+    janela = ctk.CTkToplevel(app)
+    janela.title("Alterar Atributo")
+    janela.geometry("300x400")
+    janela.grab_set()
+    label = ctk.CTkLabel(janela, text=locais[indice].capitalize() + ": " + linhas[indice].strip(), anchor="w")
+    label.pack(pady=10, fill="x", padx=10)
+    entrada = ctk.CTkEntry(janela, placeholder_text="Valor")
+    entrada.pack(pady=10)
+    botao = ctk.CTkButton(janela, text="Alterar", command=lambda: salvar(entrada.get()))
+    botao.pack(pady=10)
+
+def editar_ficha_toda():
+    janela = ctk.CTkToplevel(app)
+    janela.title("Criar/Alterar ficha")
+    janela.geometry("300x400")
+    janela.grab_set()
+    label_titulo = ctk.CTkLabel(janela, text="Criar/Alterar ficha")
+    label_titulo.pack(pady=10)
+
+    scroll = ctk.CTkScrollableFrame(janela)
+    scroll.pack(fill="both", expand=True, padx=10)
+
+    entries = []
+    for i, campo in enumerate(locais):
+        label = ctk.CTkLabel(scroll, text=campo.capitalize())
+        label.pack(anchor="w")
+
+        entry = ctk.CTkEntry(scroll, placeholder_text=campo.capitalize())
+        if i < len(linhas):
+            entry.insert(0, linhas[i].strip())
+        entry.pack(fill="x", pady=(0, 8))
+
+        entries.append(entry)
+    def salvar():
+        global linhas
+        with open(ficha, 'w') as f:
+            for entry in entries:
+                f.write(entry.get() + '\n')
+        linhas = pegar_ficha()
+        mostrar_atributos()
+        janela.destroy()
+
+    botao_ok = ctk.CTkButton(janela, text="OK", command=salvar)
+    botao_ok.pack(pady=10)
+
+if Path(ficha).is_file():
+    linhas = pegar_ficha()   
+
+ctk.set_appearance_mode("System")
+ctk.set_default_color_theme("blue")
+
+app = ctk.CTk()
+app.title("Administrador de ficha (WAR)")
+
+frame_atributos = ctk.CTkScrollableFrame(master=app)
+frame_atributos.pack(pady=20, padx=(20, 10), fill="both", expand=True, side="left")
+
+frame_controles = ctk.CTkScrollableFrame(master=app)
+
+frame_mudar_vida = ctk.CTkFrame(frame_controles, border_width=2, border_color="gray")
+frame_mudar_vida.pack(pady=10, padx=10, ipadx=10, ipady=10)
+ctk.CTkLabel(frame_mudar_vida, text="Alterar Vida", anchor="w").pack(pady=(5, 0), fill="x", padx=10)
+entrada_vida = ctk.CTkEntry(frame_mudar_vida, placeholder_text="Valor")
+entrada_vida.pack(pady=10)
+botao_vida = ctk.CTkButton(frame_mudar_vida, text="Mudar Vida", command=lambda: alterar_atributo_valor(2, entrada_vida.get()))
+botao_vida.pack(pady=10)
+
+frame_mudar_sanidade = ctk.CTkFrame(frame_controles, border_width=2, border_color="gray")
+frame_mudar_sanidade.pack(pady=10, padx=10, ipadx=10, ipady=10)
+ctk.CTkLabel(frame_mudar_sanidade, text="Alterar Sanidade", anchor="w").pack(pady=(5, 0), fill="x", padx=10)
+entrada_sanidade = ctk.CTkEntry(frame_mudar_sanidade, placeholder_text="Valor")
+entrada_sanidade.pack(pady=10)
+botao_sanidade = ctk.CTkButton(frame_mudar_sanidade, text="Mudar Sanidade", command=lambda: alterar_atributo_valor(4, entrada_sanidade.get()))
+botao_sanidade.pack(pady=10)
+
+frame_mudar_atributo = ctk.CTkFrame(frame_controles, border_width=2, border_color="gray")
+frame_mudar_atributo.pack(pady=10, padx=10, ipadx=10, ipady=10)
+ctk.CTkLabel(frame_mudar_atributo, text="Alterar Atributo", anchor="w").pack(pady=(5, 0), fill="x", padx=10)
+selecao = ctk.CTkOptionMenu(frame_mudar_atributo, values=locais)
+selecao.pack(pady=10)
+mudar_atributo_button = ctk.CTkButton(frame_mudar_atributo, text="Mudar Atributo", command=lambda: alterar_atributo(locais.index(selecao.get())))
+mudar_atributo_button.pack(pady=10)
+
+frame_mudar_ficha_toda = ctk.CTkFrame(frame_controles, border_width=2, border_color="gray")
+frame_mudar_ficha_toda.pack(pady=10, padx=10, ipadx=10, ipady=10)
+ctk.CTkLabel(frame_mudar_ficha_toda, text="Mudar Ficha Toda", anchor="w").pack(pady=(5, 0), fill="x", padx=10)
+botao_mudar_ficha = ctk.CTkButton(frame_mudar_ficha_toda, text="Mudar Atributo", command=lambda: editar_ficha_toda())
+botao_mudar_ficha.pack(pady=10)
+
+frame_controles.pack(pady=20, padx=(10, 20), fill="both", expand=True, side="left")
 
 if (Path(ficha).is_file() == False):
     editar_ficha_toda()
